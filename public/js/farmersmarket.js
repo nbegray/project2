663ee47e4a04a6950b77786e8f432477b31964ff
Baruch Hashem <barukhashem@gmail.com>
2019-03-05 15:06:33 -0700

    fixed conflict in farmersmarket.js

--- public/js/farmersmarket.js
+++ public/js/farmersmarket.js
@@ -1,53 +1,52 @@
-$(document.ready, function () {
-    $(function () {
-        $(".location-splash").on("click", function (event) {
-            $("#search-btn").on("click", function (event) {
-                event.preventDefault();
-
-                var id = $(this).data("id");
-                var marketName = $(this).data("marketName");
-                var city = $(this).data("city");
-                var state = $(this).data("state");
-                var website = $(this).data("website");
-
-                var farmersMarketdata = {
-                    id: id,
-                    marketName: marketName,
-                    city: city,
-                    state: state,
-                    website: website,
-
-                };
-
-
-
+$(document.ready, function (){
+    $(function() {
+        $("#search-btn").on("click", function (event) {
+            event.preventDefault();
+    
+            var id = $(this).data("id");
+            var marketName = $(this).data("marketName");
+            var city = $(this).data("city");
+            var state = $(this).data("state");
+            var website = $(this).data("website");
+    
+            var farmersMarketdata = {
+                id: id,
+                marketName: marketName,
+                city: city,
+                state: state,
+                website: website,
+    
+            };
+    
+            
+    
                 var zip = $("#zipCode").val().trim();
                 getResults(zip);
-
-                function getResults(zip) {
+            
+            function getResults(zip) {
+                // or
+                // function getResults(lat, lng) {
+                $.ajax({
+                    type: "GET",
+                    contentType: "application/json; charset=utf-8",
+                    // submit a get request to the restful service zipSearch or locSearch.
+                    url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
                     // or
-                    // function getResults(lat, lng) {
-                    $.ajax({
-                        type: "GET",
-                        contentType: "application/json; charset=utf-8",
-                        // submit a get request to the restful service zipSearch or locSearch.
-                        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
-                        // or
-                        // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng,
-                        dataType: 'jsonp',
-                        jsonpCallback: 'searchResultshandler',
-                        data: farmersMarketdata,
-
-
-
-                    }).then(
-                        function () {
-                            console.log("data retrieved", farmersMarketdata);
-                            searchResultsHandler()
-                            // Reload the page to get the updated list
-                            window.location.pathname = "/";
-                        })
-                    };
+                    // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng,
+                    dataType: 'jsonp',
+                    jsonpCallback: 'searchResultshandler',
+                    data: farmersMarketdata,
+    
+                
+    
+                }).then(
+                    function() {
+                      console.log("data retrieved", farmersMarketdata);
+                      searchResultsHandler()
+                      // Reload the page to get the updated list
+                      window.location.pathname = "/";
+                    })
+                };
                     function searchResultsHandler(farmersMarketdata) {
                         for (var key in farmersMarketdata) {
                             alert(key);
@@ -65,4 +64,4 @@ $(document.ready, function () {
                     }
                 })
             });
-        });
+        });    
