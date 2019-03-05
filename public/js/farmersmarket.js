4793f306a3939a6b1f6894e19d53189e7ade4220
Natalie Edmonson <natalieedmonson@Natalies-MacBook-Air.local>
2019-03-05 10:34:06 -0700

    updated api routes for farmersmarket and public farmersmarket JS

---
+++ public/js/farmersmarket.js
@@ -0,0 +1,48 @@
+function getResults(zip) {
+    // or
+    // function getResults(lat, lng) {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        // submit a get request to the restful service zipSearch or locSearch.
+        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+        // or
+        // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng,
+        dataType: 'jsonp',
+        jsonpCallback: 'searchResultsHandler'
+    });
+}
+//iterate through the JSON result object.
+function searchResultsHandler(searchresults) {
+    for (var key in searchresults) {
+        alert(key);
+        var results = searchresults[key];
+        for (var i = 0; i < results.length; i++) {
+            var result = results[i];
+            for (var key in result) {
+                //only do an alert on the first search result
+                if (i == 0) {
+                    alert(result[key]);
+                }
+            }
+        }
+    }
+}
+function getDetails(id) {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        // submit a get request to the restful service mktDetail.
+        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
+        dataType: 'jsonp',
+        jsonpCallback: 'detailResultHandler'
+    });
+}
+//iterate through the JSON result object.
+function detailResultHandler(detailresults) {
+    for (var key in detailresults) {
+        alert(key);
+        var results = detailresults[key];
+        alert(results['GoogleLink']);
+    }
+}
