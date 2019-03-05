ad5925380a17a7aa198a64508e5862128e6fac71
Blaney83 <professionallaney@gmail.com>
2019-03-05 13:06:48 -0700

    Fixed server and sequelize init files.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var htmlRoutes = require("./routes/htmlRoutes");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
