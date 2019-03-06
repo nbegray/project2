09eb2c380370b147de5e026500a61784934b9110
Blaney83 <professionallaney@gmail.com>
2019-03-05 22:31:13 -0700

    search.html and search.js playing nicely again. Added notes for additional work to be done.

--- public/js/farmersmarket.js
+++ public/js/farmersmarket.js
@@ -1,28 +1,23 @@
-$(document.ready, function (){
-    $(function() {
+$(document).ready(function () {
+    $(function () {
         $("#search-btn").on("click", function (event) {
             event.preventDefault();
-    
-            var id = $(this).data("id");
-            var marketName = $(this).data("marketName");
-            var city = $(this).data("city");
-            var state = $(this).data("state");
-            var website = $(this).data("website");
-    
-            var farmersMarketdata = {
-                id: id,
-                marketName: marketName,
-                city: city,
-                state: state,
-                website: website,
-    
-            };
-    
-            
-    
-                var zip = $("#zipCode").val().trim();
-                getResults(zip);
-            
+
+            // var id = $(this).data("id");
+            // var marketName = $(this).data("marketName");
+            // var city = $(this).data("city");
+            // var state = $(this).data("state");
+            // var website = $(this).data("website");
+
+            // var farmersMarketdata = {
+            //     id: id,
+            //     marketName: marketName,
+            //     city: city,
+            //     state: state,
+            //     website: website,
+            // };
+            var zip = $("#zipCode").val().trim();
+            getResults(zip);
             function getResults(zip) {
                 // or
                 // function getResults(lat, lng) {
@@ -36,32 +31,30 @@ $(document.ready, function (){
                     dataType: 'jsonp',
                     jsonpCallback: 'searchResultshandler',
                     data: farmersMarketdata,
-    
-                
-    
                 }).then(
-                    function() {
-                      console.log("data retrieved", farmersMarketdata);
-                      searchResultsHandler()
-                      // Reload the page to get the updated list
-                      window.location.pathname = "/";
+                    function () {
+                        console.log("data retrieved", farmersMarketdata);
+                        searchResultsHandler()
+                        // Reload the page to get the updated list
+                        window.location.pathname = "/";
                     })
-                };
-                    function searchResultsHandler(farmersMarketdata) {
-                        for (var key in farmersMarketdata) {
-                            alert(key);
-                            var results = farmersMarketdata[key];
-                            for (var i = 0; i < results.length; i++) {
-                                var result = results[i];
-                                for (var key in result) {
-                                    //only do an alert on the first search result
-                                    if (i == 0) {
-                                        alert(result[key]);
-                                    }
-                                }
+            };
+            function searchResultsHandler(farmersMarketdata) {
+                for (var key in farmersMarketdata) {
+                    alert(key);
+                    var results = farmersMarketdata[key];
+                    for (var i = 0; i < results.length; i++) {
+                        var result = results[i];
+                        for (var key in result) {
+                            //only do an alert on the first search result
+                            if (i === 0) {
+                                alert(result[key]);
                             }
                         }
                     }
-                })
-            });
-        });    
+                }
+            }
+        });
+    });
+}
+);    
--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,47 @@
 $(document).ready(function () {
+
+    $("#search-btn").on("click", function (event) {
+        event.preventDefault();
+        var zip = $("#zipCode").val().trim();
+        getResults(zip);
+        function getResults(zip) {
+            $.ajax({
+                type: "GET",
+                contentType: "application/json; charset=utf-8",
+                // submit a get request to the restful service zipSearch or locSearch.
+                url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+                // or
+                // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng,
+                dataType: 'jsonp',
+                jsonpCallback: 'searchResultshandler',
+            }).then(function (res) {
+                    console.log(res)
+                    searchResultsHandler(res);
+
+                    //TODO: use J-Query to append results to table
+                    //NOTE: make sure they include a button to "recommend" each market
+                    // for the appended elements, write a listener that will send an ajax call to the server
+                    // with the information for that market to save it into the db
+                })
+        }
+        function searchResultsHandler(farmersMarketdata) {
+            for (var key in farmersMarketdata) {
+                alert(key);
+                var results = farmersMarketdata[key];
+                for (var i = 0; i < results.length; i++) {
+                    var result = results[i];
+                    for (var key in result) {
+                        //only do an alert on the first search result
+                        if (i === 0) {
+                            alert(result[key]);
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+
     $("#add-btn").on("click", function (event) {
         event.preventDefault();
         var marketNameInput = $("#market-name-input").val();
