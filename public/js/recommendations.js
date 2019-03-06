efa21ee64cb267b6a41ad02c301fa9c7862eca57
Blaney83 <professionallaney@gmail.com>
2019-03-06 14:33:37 -0700

    merging.

--- public/js/recommendations.js
+++ public/js/recommendations.js
@@ -1,22 +1,22 @@
 
 //document .on load fires when the page is rendered
-$(document).on("load", function(){
-    $.ajax({
-        method: "GET",
-        url: "/api/recommendations",
-        dataType: 'jsonp'
-    }).then(function (res) {
-        console.log(res)
-        var results = res;
-        results.forEach(function (marketResult) {
-            //append tables here
-            var tableRow = "<tr>";
-            tableRow += "<td>" + marketResult.marketname + "</td>";
-            tableRow += "<td>" + zip + "</td>";
-            tableRow += '<td class="rating" id="' + marketResult.id + '>' + 'Rate' + '</td>';
-            tableRow += '<td class="moreInfo" id="' + marketResult.id + '>' + 'More Info' + '</td>';
-            tableRow += "</tr>";
-            $("tbody").append(tableRow);
-        }) 
-    });
+var append = "";
+$.ajax({
+    method: "GET",
+    url: "/api/recommendations",
+    dataType: 'jsonp'
+}).then(function (res) {
+    console.log(res)
+    var results = res;
+    results.forEach(function (marketResult) {
+        //append tables here
+        var tableRow = "<tr>";
+        tableRow += "<td>" + marketResult.dataValues.marketname + "</td>";
+        tableRow += "<td>" + zip + "</td>";
+        tableRow += '<td class="rating" id="' + marketResult.id + '>' + 'Rate' + '</td>';
+        tableRow += '<td class="moreInfo" id="' + marketResult.id + '>' + 'More Info' + '</td>';
+        tableRow += "</tr>";
+        $("tbody").append(tableRow);
+        append += tableRow;
+    }) 
 });
--- public/js/yelp.js
+++ public/js/yelp.js
@@ -1,10 +1,10 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-axios.get('https://api.yelp.com/v3/businesses/{id}/reviews')
-  .then(response => {
-    console.log(response.data.url);
-    console.log(response.data.explanation);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// axios.get('https://api.yelp.com/v3/businesses/{id}/reviews')
+//   .then(response => {
+//     console.log(response.data.url);
+//     console.log(response.data.explanation);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
