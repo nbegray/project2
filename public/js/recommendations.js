15b33966b64999b4066ed2241baf36b45cb20422
Blaney83 <professionallaney@gmail.com>
2019-03-07 13:07:14 -0700

    Fixed selectors, db storage and transferred code to recommendations

--- public/js/recommendations.js
+++ public/js/recommendations.js
@@ -15,10 +15,56 @@ $(document).ready(function () {
             var tableRow = "<tr>";
             tableRow += "<td>" + marketResult.marketName + "</td>";
             tableRow += "<td>" + "zip" + "</td>";
-            tableRow += '<td>' + '<button class="rating" id="' + "marketResult.dataValues.id" + '">Rate</button></td>';
-            tableRow += '<td>' + '<button class="moreInfo" id="' + "marketResult.dataValues.id "+ '">More Info</button></td>';
+            tableRow += '<td>' + '<button class="rating" id="' + marketResult.info + '">Rate</button></td>';
+            tableRow += '<td>' + '<button class="moreInfo" id="' + marketResult.info + '">More Info</button></td>';
             tableRow += "</tr>";
             $("tbody").append(tableRow);
         })
     });
+
+
+
+
+    //this is another way to write an event listener which will work on elements created via JavaScript
+    $(document).on("click", ".moreInfo", function (e) {
+        event.preventDefault();
+        console.log("working")
+        var marketdetails = e.target.id;
+        getDetails(marketdetails);
+
+
+        function getDetails(marketdetails) {
+            $.ajax({
+                type: "GET",
+                contentType: "application/json; charset=utf-8",
+                // submit a get request to the restful service mktDetail.
+                url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + marketdetails,
+                dataType: 'jsonp',
+                jsonpCallback: 'detailResultHandler'
+            }).then(function (res) {
+                console.log(res)
+                detailResultsHandler(res);
+            })
+            //iterate through the JSON result object.
+            function detailResultsHandler(farmersmarket) {
+                console.log(farmersmarket.marketdetails)
+                var results = farmersmarket.marketdetails
+                var detailRow = "<tr>";
+                detailRow += "<td>" + results.Address + "</td>";
+                detailRow += "<td><a href='" + results.GoogleLink + "'>Google</a></td>";
+                detailRow += "<td>" + results.Products + "</td>";
+                detailRow += "<td>" + results.Schedule + "</td>";
+                detailRow += "</tr>";
+
+                $("#details-here").empty();
+
+                $("#details-here").append(detailRow);
+                $(".modal").modal("show");
+
+
+            }
+        }
+
+
+    })
 })
